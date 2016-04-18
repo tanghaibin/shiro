0c8279e31b0362cdf63ee4880eab763fa3555367
tanghaibin <tanghaibinhaohan@163.com>
2016-04-18 17:57:25 +0800

    commit

--- src/main/java/top/tanghaibin/context/UserRealm.java
+++ src/main/java/top/tanghaibin/context/UserRealm.java
@@ -3,9 +3,11 @@ package top.tanghaibin.context;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import top.tanghaibin.entity.User;
 
 /**
  * Created by Administrator on 2016/4/18.
@@ -18,6 +20,12 @@ public class UserRealm extends AuthorizingRealm {
 
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        String username = authenticationToken.getPrincipal().toString();
+        String password = authenticationToken.getCredentials().toString();
+//        if("tanghaibin".equals(user.getUsername()) && "123".equals(user.getPswd())){
+//            return new SimpleAuthenticationInfo(user.getUsername(), user.getPswd(), getName());
+//        }
+
         return null;
     }
 }
